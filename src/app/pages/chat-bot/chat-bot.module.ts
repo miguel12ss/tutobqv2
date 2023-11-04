3f7895b41be29053741888c6b04ab8de2a96896d
joshua <josuacff@gmail.com>
2023-11-04 14:00:07 -0500

    chatbot medio

---
+++ src/app/pages/chat-bot/chat-bot.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ChatBotRoutingModule } from './chat-bot-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ChatBotRoutingModule
+  ]
+})
+export class ChatBotModule { }
